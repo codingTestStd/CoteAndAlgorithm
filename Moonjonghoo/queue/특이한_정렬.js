1d1ce1d744c5911f170d6e496c584ddf5e3b0c87
moonjonghoo <kafmjh12@gmail.com>
2025-01-19 15:36:03 +0900

    프로세스 /고급 수정

--- Moonjonghoo/queue/특이한_정렬.js
+++ Moonjonghoo/queue/특이한_정렬.js
@@ -4,8 +4,8 @@ function solution(numlist, n) {
     const diffB = Math.abs(b - n);
 
     if (diffA === diffB) {
-      return b - a; // 거리가 같으면 큰 숫자가 우선
+      return b - a; 
     }
-    return diffA - diffB; // 거리 기준으로 오름차순 정렬
+    return diffA - diffB; 
   });
 }
--- Moonjonghoo/queue/프로세스.js
+++ Moonjonghoo/queue/프로세스.js
@@ -1,16 +1,17 @@
 function solution(priorities, location) {
-  let answer = 0;
-  let test = [];
+  let order = 0;
+
   while (priorities.length > 0) {
     const current = priorities.shift();
     if (priorities.some((priority) => priority > current)) {
       priorities.push(current);
-      if (location > 0) {
-        location--;
-      }
+      location = location === 0 ? priorities.length - 1 : location - 1;
     } else {
-      test.push(current);
+      order++;
+      if (location === 0) {
+        return order; // 목표 프로세스가 실행되었을 때 순서를 반환
+      }
+      location--;
     }
   }
-  return location + 1;
 }
