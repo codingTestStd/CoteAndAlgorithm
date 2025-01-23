cf0bad6efff5a696717b134fd8112d3494817b27
leeyoul <2yulrang@gmail.com>
2025-01-23 18:32:43 +0900

    [yulrang] 25.01.19 (#31)
    * Slice array / 기초
    
    * Length of array items / 기초
    
    * Shift array / 기초
    
    * Number of duplicated item / 기초
    
    * Remove lowest number / 중급
    
    * Sum of matrix / 중급
    
    * Multiple number / 중급
    
    * Chore: 폴더명 변경경
    
    * 문자열 뒤집기 / 기초
    
    * 컨트롤 제트 / 기초
    
    * 영어는 어려워 / 기초
    
    * 문자열 계산하기 / 기초
    
    * 크레인 인형뽑기 게임 / 중급
    
    * refactor: 크레인 인형뽑기 게임 / 중급
    
    * 올바른 괄호 / 중급
    
    * 다트 게임 / 중급
    
    * 순서쌍의 개수 / 기초
    
    * 점의 위치 구하기 / 기초
    
    * 로그인 성공? / 기초
    
    * 특이한 정렬 / 기초
    
    * 카드 뭉치 / 중급
    
    * 공원 산책 / 중급
    
    * 햄버거 만들기 / 중급

---
+++ yulrang/03_Queue/07_Hamberger.js
@@ -0,0 +1,12 @@
+function solution(ingredient) {
+  var answer = 0;
+  
+  for(let i=0; i<ingredient.length; i++) {
+      if(ingredient[i] === 1 && ingredient[i+1] === 2 && ingredient[i+2] === 3 && ingredient[i+3] === 1) {
+          ingredient.splice(i, 4);
+          answer++;
+          i -= 4;
+      }
+  }
+  return answer;
+}
