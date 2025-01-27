9c3e99c522c723146e62248c5010b99149ca1dba
unn04012 <unn04012@naver.com>
2025-01-27 12:53:47 +0900

    모스부호(기초)

---
+++ unn04012/hash/morse.js
@@ -0,0 +1,42 @@
+function solution(letter) {
+  let answer = '';
+  const morse = {
+    '.-': 'a',
+    '-...': 'b',
+    '-.-.': 'c',
+    '-..': 'd',
+    '.': 'e',
+    '..-.': 'f',
+    '--.': 'g',
+    '....': 'h',
+    '..': 'i',
+    '.---': 'j',
+    '-.-': 'k',
+    '.-..': 'l',
+    '--': 'm',
+    '-.': 'n',
+    '---': 'o',
+    '.--.': 'p',
+    '--.-': 'q',
+    '.-.': 'r',
+    '...': 's',
+    '-': 't',
+    '..-': 'u',
+    '...-': 'v',
+    '.--': 'w',
+    '-..-': 'x',
+    '-.--': 'y',
+    '--..': 'z',
+  };
+
+  const morses = letter.split(' ');
+
+  for (const e of morses) {
+    answer += morse[e];
+  }
+  return answer;
+}
+
+const letter = '.--. -.-- - .... --- -.';
+
+console.log(solution(letter));
